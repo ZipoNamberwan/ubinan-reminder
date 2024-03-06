707e30183ede22c5384e71bce7f79091090cddbf
Douwes Dekker <dopokan1@gmail.com>
2024-03-06 21:33:27 +0700

    reminder in one subround, add more quote

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -16,6 +16,8 @@ class MessageController extends Controller
 {
     public function index()
     {
+        $randQuoteNumMax = count(Quote::all());
+
         $transformedData = [
             'monthly' => [],
             'harvest' => [],
@@ -24,7 +26,7 @@ class MessageController extends Controller
         $breakpoints = [];
 
         $today = date("Y-m-d", strtotime('+7 hours'));
-        // $today = '2024-01-01';
+        // $today = '2024-03-07';
 
         $firstOfNextMonth = date("Y-m-d", strtotime("+7 hours first day of next month"));
         $firstbreakpoints = date("Y-m-d", strtotime("+7 hours -7 day", strtotime($firstOfNextMonth)));
@@ -98,9 +100,9 @@ class MessageController extends Controller
                     $ppl = User::find($pplId);
                     $prefixppl = '';
                     if ($selectedBreakpoint[1] == 'next') {
-                        $prefixppl = "Selamat pagi, *Ubinan Fighters….!!!*🤩🤩🤩 \r\n\r\nBulan depan (*" . $month->name . "*) Anda punya *" . count($schedules) . " sampel ubinan*. Berikut daftar sampel ubinan bulan depan 💪💪💪 \r\n \r\n";
+                        $prefixppl = "Halo, *Ubinan Fighters….!!!*🤩🤩🤩 \r\n\r\nBulan depan (*" . $month->name . "*) Anda punya *" . count($schedules) . " sampel ubinan*. Berikut daftar sampel ubinan bulan depan 💪💪💪 \r\n \r\n";
                     } else {
-                        $prefixppl = "Selamat pagi, *Ubinan Fighters….!!!*🤩🤩🤩 \r\n\r\nMengingaatkan kembali, bulan ini (*" . $month->name . "*) Anda masih punya *" . count($schedules) . " sampel ubinan yang belum diisi tanggal perkiraan panennya*. Berikut daftar sampel ubinan tersebut 💪💪💪 \r\n \r\n";
+                        $prefixppl = "Halo, *Ubinan Fighters….!!!*🤩🤩🤩 \r\n\r\nMengingaatkan kembali, bulan ini (*" . $month->name . "*) Anda masih punya *" . count($schedules) . " sampel ubinan yang belum diisi tanggal perkiraan panennya*. Berikut daftar sampel ubinan tersebut 💪💪💪 \r\n \r\n";
                     }
                     $pplMessage = implode("\r\n", $samples);
                     $suffixppl = "\r\n\r\nMohon segera melakukan *input perkiraan tanggal panen* untuk sampel tersebut melalui link➡️ \r\n\r\n" . url("/jadwal-ubinan?month=" . $month->id) . " \r\n\r\n*Semangat Ubinan*, Huha…😸😸\r\n\r\nNb: Pesan 💚 Khusus untuk: *" . $ppl->name . "*";
@@ -110,21 +112,23 @@ class MessageController extends Controller
                 }
                 $prefixpml = '';
                 if ($selectedBreakpoint[1] == 'next') {
-                    $prefixpml = "Selamat pagi, *PML Ubinan Fighters….!!!*🤩🤩🤩 \r\n\r\nBerikut adalah jumlah sampel ubinan bulan depan (*" . $month->name . "*) untuk PPL Anda 💪💪💪 \r\n \r\n";
+                    $prefixpml = "Halo, *PML Ubinan Fighters….!!!*🤩🤩🤩 \r\n\r\nBerikut adalah jumlah sampel ubinan bulan depan (*" . $month->name . "*) untuk PPL Anda 💪💪💪 \r\n \r\n";
                 } else {
-                    $prefixpml = "Selamat pagi, *PML Ubinan Fighters….!!!*🤩🤩🤩 \r\n\r\nBerikut adalah jumlah sampel ubinan bulan ini (*" . $month->name . "*) yang belum diinput tanggal perkiraan panennya oleh PPL 💪💪💪 \r\n \r\n";
+                    $prefixpml = "Halo, *PML Ubinan Fighters….!!!*🤩🤩🤩 \r\n\r\nBerikut adalah jumlah sampel ubinan bulan ini (*" . $month->name . "*) yang belum diinput tanggal perkiraan panennya oleh PPL 💪💪💪 \r\n \r\n";
                 }
                 $pmlMessage = '';
                 foreach ($pplmap as $map) {
-                    $pmlMessage = $pmlMessage . "🚩*" . $map['name'] . "* : *" . $map['total_sample'] . "* sampel" . "\r\n";
+                    $pmlMessage = $pmlMessage . "🚩*" . $map['name'] . "* : *" . $map['total_sample'] . "* sampel" . "\r\n\r\n";
                 }
-                $suffixpml = "Mohon untuk mengingatkan kembali PPL untuk menginput perkiraan tanggal panen. Sampel selengkapnya dan perkiraan tanggal panen bisa diakses melalui link berikut➡️ \r\n\r\n" . url("/jadwal-panen") . " \r\n\r\nNb: Pesan 💚 Khusus untuk: *" . $pml->name . "*";
+                $rand = rand(1, $randQuoteNumMax);
+                $quote = Quote::find($rand)->quote;
+                $suffixpml = "Mohon untuk mengingatkan kembali PPL untuk menginput perkiraan tanggal panen. Sampel selengkapnya dan perkiraan tanggal panen bisa diakses melalui link berikut➡️ \r\n\r\n" . url("/jadwal-panen") . " \r\n\r\nNb: Pesan 💚 Khusus untuk: *" . $pml->name . "*\r\nBonus kata-kata mutiara:\r\n" . $quote;
 
                 $message[] = ["message" => $prefixpml . $pmlMessage . $suffixpml, "phone_number" => "+62" . $pml->phone_number, "type" => "monthly", "sent_to" => $pml->name, "ids" => [], "role" => $pml->roles->first()->name];
             }
 
             $admin = User::find(1);
-            $prefixadmin = "Selamat pagi *Admin Survei Ubinan* 🤩🤩🤩\r\n\r\n";
+            $prefixadmin = "Halo *Admin Survei Ubinan* 🤩🤩🤩\r\n\r\n";
             $adminMessage = '';
             if ($selectedBreakpoint[1] == 'next') {
                 $adminMessage = "Hari ini telah dikirimkan Pengingat Bulanan untuk bulan *" . $month->name . "* kepada petugas Survei Ubinan. Secara keseluruhan ada total *" . $totalSample . " sampel*. Selanjutnya PPL akan mengisi jadwal panen untuk setiap sampel. Selengkapnya bisa dilihat di➡️ \r\n\r\n"
@@ -143,8 +147,13 @@ class MessageController extends Controller
         $gapDay = ['+1' => 'Besok', '+3' => '3 Hari Lagi'];
         $mapGapDay = [];
         $year = Year::where(['name' => date('Y', strtotime($today))])->first();
-        $month = Month::find(intval(date("m", strtotime($today))));
-        $schedules = MonthlySchedule::where('year_id', $year->id)->where('month_id', $month->id)->orderBy(HarvestSchedule::select('date')->whereColumn('harvest_schedules.monthly_schedule_id', 'monthly_schedules.id'))->get();
+        // $month = Month::find(intval(date("m", strtotime($today))));
+        $subround = (int) (floor((intval(date("m", strtotime($today))) - 1) / 4) + 1);
+        $max = $subround * 4;
+        $min = $max - 3;
+        $months = range($min, $max);
+
+        $schedules = MonthlySchedule::where('year_id', $year->id)->whereIn('month_id', $months)->orderBy(HarvestSchedule::select('date')->whereColumn('harvest_schedules.monthly_schedule_id', 'monthly_schedules.id'))->get();
         foreach ($gapDay as $gap => $alias) {
             $mapGapDay[$gap] = ['date' => date("Y-m-d", strtotime("+" . $gap . " day", strtotime($today))), 'alias' => $alias];
         }
@@ -162,7 +171,6 @@ class MessageController extends Controller
                 }
             }
 
-            $randQuoteNumMax = count(Quote::all());
             foreach ($transformedData['harvest'] as $pmlId => $pplschedules) {
                 $pml = User::find($pmlId);
                 $pplmap = [];
@@ -193,22 +201,24 @@ class MessageController extends Controller
 
                     $message[] = ["message" => $prefixppl . $pplMessage . $suffixppl, "phone_number" => "+62" . $ppl->phone_number, "type" => "harvest", "sent_to" => $ppl->name, "ids" => $scheduleIds, "role" => $ppl->roles->first()->name];
                 }
-                $prefixpml = "Selamat Pagi *PML Ubinan Fighters…*🤩🤩🤩, \r\nMengingatkan kembali kalau dalam waktu dekat ada panen untuk sampel ubinan berikut:\r\n\r\n";
+                $prefixpml = "Halo *PML Ubinan Fighters…*🤩🤩🤩, \r\nMengingatkan kembali kalau dalam waktu dekat ada panen untuk sampel ubinan berikut:\r\n\r\n";
                 $pmlMessage = '';
                 foreach ($pplmap as $key => $array) {
                     $pmlMessage = $pmlMessage . strtoupper($gapDay[$key]) . "\r\n";
                     $pmlMessage = $pmlMessage . implode("\r\n", $array);
                     $pmlMessage = $pmlMessage . "\r\n\r\n";
                 }
+                $rand = rand(1, $randQuoteNumMax);
+                $quote = Quote::find($rand)->quote;
                 $suffixpml = "\r\nMohon untuk *mengingatkan* kembali PPL terkait jadwal panen tersebut agar tidak terlewat. Sampel selengkapnya dan perkiraan tanggal panen bisa diakses melalui link berikut➡️ \r\n\r\n" . url("/jadwal-panen") .
-                    " \r\n\r\n*Semangat, Fighting*💪💪💪";
+                    " \r\n\r\n*Semangat, Fighting*💪💪💪.\r\nBonus kata-kata mutiara:\r\n" . $quote;
 
                 $message[] = ["message" => $prefixpml . $pmlMessage . $suffixpml, "phone_number" => "+62" . $pml->phone_number, "type" => "harvest", "sent_to" => $pml->name, "ids" => [], "role" => $pml->roles->first()->name];
             }
 
             if (count($transformedData['harvest']) > 0) {
                 $admin = User::find(1);
-                $prefixadmin = "Selamat pagi *Admin Survei Ubinan* 🤩🤩🤩\r\n\r\n";
+                $prefixadmin = "Halo *Admin Survei Ubinan* 🤩🤩🤩\r\n\r\n";
                 $adminMessage = "Hari ini telah dikirimkan reminder jadwal panen\r\n";
                 foreach ($gapDay as $key => $day) {
                     $adminMessage = $adminMessage . "🚩 *" . strtoupper($day) . "* ada *" . $messageCount[$key] . "* sampel\r\n";
@@ -222,7 +232,7 @@ class MessageController extends Controller
         if (count($transformedData['monthly']) == 0 && count($transformedData['harvest']) == 0) {
             $admin = User::find(1);
 
-            $prefixadmin = "Selamat pagi *Admin Survei Ubinan* 🤩🤩🤩\r\n\r\n";
+            $prefixadmin = "Halo *Admin Survei Ubinan* 🤩🤩🤩\r\n\r\n";
             $adminMessage = "Hari ini tidak ada reminder jadwal ubinan\r\n";
             $suffixadmin = "\r\n\r\n*Terima kasih...*💪💪💪";
             $message[] = ["message" => $prefixadmin . $adminMessage . $suffixadmin, "phone_number" => "+62" . $admin->phone_number, "type" => "harvest", "sent_to" => $admin->name, "ids" => [], "role" => $admin->roles->first()->name];
